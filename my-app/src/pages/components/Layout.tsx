ddce9c90c9fdf6880ffb08c2e83c4af053118696
UBlandine <124615325+UBlandine@users.noreply.github.com>
2025-02-04 12:49:56 +0200

    Added api foldel and all installation for backend

---
+++ my-app/src/pages/components/Layout.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from "react";
+import {Navbar} from "../components/navbar";
+import Footer from "./Footer copy";
+
+interface LayoutProps {
+  children: ReactNode;
+  showSidebar?: boolean;
+}
+
+const Layout = ({ children, showSidebar = false }: LayoutProps) => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <main className="flex-1">{children}</main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
